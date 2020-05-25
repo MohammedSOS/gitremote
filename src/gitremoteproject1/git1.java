3d0124188c8de77c95b921b310fdbb2ea872788f
mohamed <jenkinsppeline05@gmail.com>
2020-05-25 14:28:52 +0100

    First Commit.....

---
+++ src/gitremoteproject1/git1.java
@@ -0,0 +1,15 @@
+package gitremoteproject1;
+
+public class git1 {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes"){
+			System.out.println("eat somthing ....");
+		}
+		else {System.out.println("do work ....");
+		}
+
+	}
+
+}
